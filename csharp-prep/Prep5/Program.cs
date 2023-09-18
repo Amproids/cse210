3b0dbe59b8bca74c9d5e4e164976c4946ebfc0f1
Andrew Parry <parryandrew47@gmail.com>
2023-09-17 23:16:31 -0600

    Finished prep 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,31 @@
 using System;
+using System.Globalization;
 
 class Program
 {
+    static void DisplayWelcome() {
+        Console.WriteLine("Welcome to the Program!");
+    }
+    static string PromptUserName() {
+        Console.Write("Enter your name: ");
+        return Console.ReadLine();
+    }
+    static int PromptUserNumber() {
+        Console.Write("Enter your number: ");
+        return int.Parse(Console.ReadLine());
+    }
+    static int SquareNumber(int number) {
+        return number * number;
+    }
+    static void DisplayResult(string name, int number) {
+        Console.WriteLine($"{name}, the square of your number is {number}");
+    }
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string userName = PromptUserName();
+        int userNumber = PromptUserNumber();
+        int squaredNumber = SquareNumber(userNumber);
+        DisplayResult(userName, squaredNumber);
     }
 }
