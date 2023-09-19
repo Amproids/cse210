f0eeb0c8798f83d9329f4069125065f2b900a7cf
Andrew Parry <parryandrew47@gmail.com>
2023-09-18 23:12:27 -0600

    Finished W02 Learning Activity :)

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,26 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Salesman";
+        job1._company = "KFC";
+        job1._startYear = 2020;
+        job1._endYear = 2021;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Software Engineer";
+        job2._company = "Valve";
+        job2._startYear = 2021;
+        job2._endYear = 2023;
+
+        Resume myresume = new Resume();
+        myresume._name = "Andrew Parry";
+        myresume._Jobs.Add(job1);
+        myresume._Jobs.Add(job2);
+        myresume.Display();
     }
 }
