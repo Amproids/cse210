e908ea7f50f83b49b04820e02a425888d5dc2d3a
Andrew <parryandrew47@gmail.com>
2023-09-29 10:01:46 -0600

    Added demo for reading from text file

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        string filename = "Journal.txt";
+        string[] lines = System.IO.File.ReadAllLines(filename);
+
+        foreach (string line in lines)
+        {
+            string[] parts = line.Split(",");
+
+            string header = parts[0];
+            string entry = parts[1];
+            Console.WriteLine($"Header: {header}");
+            Console.WriteLine($"Entry:  {entry}");
+        }
     }
 }
