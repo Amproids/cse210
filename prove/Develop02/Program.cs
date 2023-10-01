47eade7c222bd48d4d17e7b5d78527987362fe7c
Andrew Parry <parryandrew47@gmail.com>
2023-09-30 18:47:19 -0600

    Added starting components for menu.

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,17 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        string filename = "Journal.txt";
-        string[] lines = System.IO.File.ReadAllLines(filename);
-
-        foreach (string line in lines)
-        {
-            string[] parts = line.Split(",");
-
-            string header = parts[0];
-            string entry = parts[1];
-            Console.WriteLine($"Header: {header}");
-            Console.WriteLine($"Entry:  {entry}");
+        Console.WriteLine("Welcome to the journaling program.");
+        bool journaling = true;
+        while (journaling) {
+            Console.WriteLine("Choose an option (1-3)");
+            Console.WriteLine("1. Make entry for today.");
+            Console.WriteLine("2. Make Entry for specific date.");
+            Console.WriteLine("3. See today's journal entry.");
+            Console.WriteLine("4. See entry for specific date.");
+            Console.WriteLine("5. See all journal entries.");
+            Console.WriteLine("6. Exit program.");
+            string menuInput = Console.ReadLine();
+            if (menuInput == "6") {
+                journaling = false;
+            }
         }
     }
 }
