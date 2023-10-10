6b6ef6aacb4e95836093c8ebbaa870ed40d84bb3
Andrew Parry <parryandrew47@gmail.com>
2023-10-09 22:34:54 -0600

    Finished w04 Learning Assignment

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment samuel = new Assignment("Samuel Bennet", "Multiplication");
+        Console.WriteLine(samuel.GetSummary());
+
+        MathAssignment roberto = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(roberto.GetSummary());
+        Console.WriteLine(roberto.GetHomeworkList());
+
+        WritingAssignment mary = new WritingAssignment("Mary Waters", "European History", "The Causes of Ward War II");
+        Console.WriteLine(mary.GetSummary());
+        Console.WriteLine(mary.GetWritingInformation());
     }
 }
