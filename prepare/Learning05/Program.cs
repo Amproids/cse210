13fc15176e38cd73af5a66dc1e7aa71ca68b2ef6
Andrew Parry <parryandrew47@gmail.com>
2023-10-13 16:18:19 -0600

    Finished W05 Learning Activity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square shape1 = new Square("Blue", 3);
+        Rectangle shape2 = new Rectangle("Yellow", 2, 5);
+        Circle shape3 = new Circle("Red", 2.5);
+
+        List<Shape> shapes = new List<Shape> {shape1, shape2, shape3};
+        foreach (Shape shape in shapes) {
+            Console.WriteLine(shape.GetColor());
+            Console.WriteLine(shape.GetArea());
+        }
     }
 }
